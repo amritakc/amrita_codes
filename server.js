2990f3482f0568939e77fdf3d90eee395d771937
amritakc <amritakc@gmail.com>
2016-05-17 12:44:57 -0700

    scrolling through pictures

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var bodyParser = require('body-parser'); 
+
+
+// var angular = require('angular');
+// var ngTouch = require('angular-touch');
+// var carousel  = require('angular-carousel');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
